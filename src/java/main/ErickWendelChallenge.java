0178b7cc6766a6a6b3b90424b6e8e343c4a0b97e
Ryan S <ryansateles@gmail.com>
2022-08-23 10:23:09 -0300

    feat: Api pagination hadling done

--- src/java/main/ErickWendelChallenge.java
+++ src/java/main/ErickWendelChallenge.java
@@ -4,13 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
-import org.json.*;
 
 public class ErickWendelChallenge {
     private ErickWendelChallenge() {
@@ -18,53 +11,51 @@ public class ErickWendelChallenge {
 
     public static void problemOne() {
         var data = readFile();
-        System.out.println(getPalindromicPrimeWithLength(9, data));
+        var found = getPalindromicPrimeWithLength(9, data);
+        System.out.println(found);
     }
 
     public static void problemTwo() {
-        var result = "no palindromic prime found";
         //get request from https://api.pi.delivery/v1/pi?start=0&numberOfDigits=100
-        var data = getDataFromApi();
-        System.out.println(data);
-        result = getPalindromicPrimeWithLength(9, data);
-        System.out.println(result);
-    }
-
-    private static String getDataFromApi() {
-        try {
-            var request = HttpRequest
-                    .newBuilder(new URI("https://api.pi.delivery/v1/pi?start=0&numberOfDigits=100"))
-                    .GET()
-                    .build();
-
-            var response = HttpClient.newBuilder()
-                    .build()
-                    .send(request, HttpResponse.BodyHandlers.ofString())
-                    .body();
-
-            var  json = new JSONObject(response);
-            return json.get("content").toString();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
+        var start = 1L;
+        var numberOfDigits = 1000;
+        var max = 100_000_000_000L;
+        var numberOfDigitsToCheck = 9;
+        //TODO use Threads to speed up the process
+        while (start < max) {
+            var data = HandlerPiApi.getDataFromApi(start, numberOfDigits);
+            var found = getPalindromicPrimeWithLength(numberOfDigitsToCheck, data);
+            if (found != null) {
+                System.out.println(found);
+                break;
+            }
+            start += numberOfDigits - numberOfDigitsToCheck + 1;
         }
-        return "";
+        if (start >= max) {
+            System.out.println("Palindromic Prime Not found");
+        } else {
+            System.out.println("Palindromic Prime found at " + start);
+        }
     }
+
 
     public static String getPalindromicPrimeWithLength(int length, String data) {
+        //TODO use Sliding Window Technique
         for (int i = 0; i < data.length() - length; i++) {
             var sub = data.substring(i, i + length);
-            if (sub.equals(new StringBuilder(sub).reverse().toString())) {
-                if (isPrime(Long.parseLong(sub))) {
-                    return sub;
-                }
+            if (isPalindromic(sub) && (isPrime(sub))) {
+                return sub;
             }
         }
-        return "No palindromic prime found";
+        return null;
     }
 
-    public static boolean isPrime(long n) {
+    private static boolean isPalindromic(String sub) {
+        return sub.equals(new StringBuilder(sub).reverse().toString());
+    }
+
+    public static boolean isPrime(String num) {
+        var n = Long.parseLong(num);
         if (n < 2)
             return false;
 
@@ -78,7 +69,7 @@ public class ErickWendelChallenge {
     public static String readFile() {
         var result = "";
         var absolutePath = new File("").getAbsolutePath();
-        var file = new File(absolutePath + "/src/java/main/oneMillionPiDigits.txt");
+        var file = new File(absolutePath + "/src/java/resources/oneMillionPiDigits.txt");
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             result = bufferedReader.readLine();
         } catch (IOException e) {
---
+++ src/java/main/HandlerPiApi.java
@@ -0,0 +1,39 @@
+package main;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class HandlerPiApi {
+    private static final String BASE_URL = "https://api.pi.delivery/v1/pi?";
+    private HandlerPiApi() {
+    }
+
+    public static String getDataFromApi(long start, int numberOfDigits) {
+        var url = BASE_URL + "start=" + start + "&numberOfDigits=" + numberOfDigits;
+        try {
+            var request = HttpRequest
+                    .newBuilder(new URI(url))
+                    .GET()
+                    .build();
+
+            var response = HttpClient.newBuilder()
+                    .build()
+                    .send(request, HttpResponse.BodyHandlers.ofString())
+                    .body();
+
+            var  json = new JSONObject(response);
+            return json.get("content").toString();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return "";
+    }
+}
